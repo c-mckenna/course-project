8acb1cee8140482116dc3fa3bb749aafa52a36ce
Callum McKenna <contact@callum.id.au>
2017-12-12 21:19:47 +1100

    Upgraded to Angular 5

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,36 +1,38 @@
-import { Http, Response } from '@angular/http';
-import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from 'app/recipes/recipe.model';
-import 'rxjs/Rx';
 import { AuthService } from 'app/auth/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.put('https://ng-recipe-book-c56da.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    return this.http.put<Recipe[]>(
+      'https://ng-recipe-book-c56da.firebaseio.com/recipes.json?auth=' + token,
+      this.recipeService.getRecipes());
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.http.get('https://ng-recipe-book-c56da.firebaseio.com/recipes.json?auth=' + token)
-      .map((response: Response) => {
-        const recipes: Recipe[] = response.json();
-
-        for (const recipe of recipes) {
-          if (!recipe['ingredients']) {
-            recipe['ingredients'] = [];
+    this.http.get<Recipe[]>('https://ng-recipe-book-c56da.firebaseio.com/recipes.json?auth=' + token)
+      .pipe(
+        map((recipes: Recipe[]) => {
+          for (const recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
           }
-        }
 
-        return recipes;
-      })
+          return recipes;
+        })
+      )
       .subscribe(
         (recipes: Recipe[]) => this.recipeService.setRecipes(recipes),
         (error) => console.log(error)
